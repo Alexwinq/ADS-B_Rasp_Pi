73262a439ed0fbc6214174eadf051a230e730e92
Alex Winquist <alexwinquist1@gmail.com>
2025-07-01 16:07:41 -0400

    init screen

--- screens/init_screen.py
+++ screens/init_screen.py
@@ -4,6 +4,7 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.utils import get_color_from_hex
 from kivy.resources import resource_add_path
+from kivy.app import App
 import os
 
 font_path = os.path.join(os.path.dirname(__file__), "..", "assets", "fonts")
@@ -12,10 +13,11 @@ resource_add_path(font_path)
 
 TAN_COLOR = get_color_from_hex("#D2B48C")
 
+
+
 class InitScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
         self.dot_index = 0
         self.dot_sequence = [".", "..", "...", "...."]
 
@@ -33,18 +35,14 @@ class InitScreen(Screen):
         layout.add_widget(self.label)
         self.add_widget(layout)
 
-    def on_enter(self):
-        self.dot_index = 0
         Clock.schedule_interval(self.animate_dots, 0.5)
-        Clock.schedule_once(self.goto_main_screen, 8)
-
-    def on_leave(self):
-        Clock.unschedule(self.animate_dots)
+        Clock.schedule_once(self.quit_app, 5)  # Quit after 5 seconds
 
     def animate_dots(self, dt):
         dots = self.dot_sequence[self.dot_index]
         self.label.text = f"Initializing{dots}"
         self.dot_index = (self.dot_index + 1) % len(self.dot_sequence)
 
-    def goto_main_screen(self, dt):
-        self.manager.current = "home"  # or whatever your next screen is
+    def quit_app(self, dt):
+        App.get_running_app().stop()
+
