ec4662514ba66159349ff454d50335a0f860a44d
Alex Winquist <alexwinquist1@gmail.com>
2025-08-22 21:57:22 -0400

    test for screen 2 once json detected

--- screens/aircraft_detect_screen.py
+++ screens/aircraft_detect_screen.py
@@ -1,21 +1,16 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.clock import Clock
+from kivy.core.text import LabelBase
 from kivy.utils import get_color_from_hex
 from kivy.resources import resource_add_path
-from kivy.uix.screenmanager import Screen
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.clock import Clock
-from kivy.core.text import LabelBase
-from kivy.core.window import Window
-from kivy.app import App
 import os
 import json
 
 font_path = os.path.join(os.path.dirname(__file__), "..", "assets", "fonts", "VT323-Regular.ttf")
 resource_add_path(font_path)
+LabelBase.register(name="VT323", fn_regular=font_path)
+
 TAN_COLOR = get_color_from_hex("#D2B48C")
 
 class AircraftDetect(Screen):
@@ -23,17 +18,22 @@ class AircraftDetect(Screen):
         super().__init__(**kwargs)
         self.layout = BoxLayout(orientation='vertical', padding=20)
         self.data_label = Label(
-            text="",  # Start empty since data should exist when this screen is shown
+            text="",
             font_size=24,
             color=TAN_COLOR,
-            font_name=font_path,
+            font_name="VT323",
             halign='left',
             valign='top'
         )
-        self.data_label.bind(size=self.data_label.setter('text_size'))
+        self.data_label.bind(size=self._update_label_height)
 
         self.layout.add_widget(self.data_label)
         self.add_widget(self.layout)
+
+    def _update_label_height(self, instance, value):
+        instance.text_size = (instance.width, None)
+        instance.texture_update()
+        instance.height = instance.texture_size[1]
 
     def update_data(self, json_data):
         formatted_text = json.dumps(json_data, indent=2)
