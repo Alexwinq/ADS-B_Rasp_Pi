f5411f7a0510f93f7af157b2986be1d39a10b8f8
Alex Winquist <alexwinquist1@gmail.com>
2025-09-03 16:03:01 -0400

    sizing updates

--- screens/aircraft_detect_screen.py
+++ screens/aircraft_detect_screen.py
@@ -1,25 +1,21 @@
-from kivy.config import Config
-Config.set('graphics', 'width', '800')
-Config.set('graphics', 'height', '480')
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.core.text import LabelBase
 from kivy.utils import get_color_from_hex
-from kivy.resources import resource_add_path
-import os
 import json
-
-font_path = os.path.join(os.path.dirname(__file__), "..", "assets", "fonts", "VT323-Regular.ttf")
-resource_add_path(font_path)
-LabelBase.register(name="VT323", fn_regular=font_path)
 
 TAN_COLOR = get_color_from_hex("#D2B48C")
 
 class AircraftDetect(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
         self.layout = BoxLayout(orientation='vertical', padding=20)
+
+        # ScrollView to enable scrolling of long JSON text
+        scrollview = ScrollView(size_hint=(1, 1))
+
         self.data_label = Label(
             text="",
             font_size=24,
@@ -27,23 +23,19 @@ class AircraftDetect(Screen):
             font_name="VT323",
             halign='left',
             valign='top',
-            size_hint_y=None
+            size_hint_y=None,  # Important: let height be dynamic
         )
-        #self.data_label.bind(size=self._update_label_height)
+        self.data_label.bind(texture_size=self.update_label_height)
 
-
-        self.layout.add_widget(self.data_label)
+        scrollview.add_widget(self.data_label)
+        self.layout.add_widget(scrollview)
         self.add_widget(self.layout)
 
-    def _update_label_height(self, instance, value):
-        instance.text_size = (instance.width, None)
-        instance.texture_update()
-        instance.height = instance.texture_size[1]
+    def update_label_height(self, instance, size):
+        instance.height = size[1]  # Make label height equal to the texture height
+        instance.text_size = (instance.width, None)  # Wrap text to label width
 
     def update_data(self, json_data):
         formatted_text = json.dumps(json_data, indent=2)
         self.data_label.text = formatted_text
 
-        self.data_label.text_size = (self.data_label.width, None)
-        self.data_label.texture_update()
-        self.data_label.height = self.data_label.texture_size[1]
