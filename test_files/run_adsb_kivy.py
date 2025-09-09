8c0298cbf45d7445117352260cdaef90595b0e37
Alex Winquist <alexwinquist1@gmail.com>
2025-09-09 16:35:09 -0400

    test for python adsb read

--- test_files/run_adsb_kivy.py
+++ test_files/run_adsb_kivy.py
@@ -1,54 +1,70 @@
-import subprocess
 import json
+import subprocess
+import threading
 import time
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.clock import Clock
 
 DUMP1090_PATH = "/usr/bin/dump1090-fa"
-JSON_DIR = "/home/alex/dump1090-json"
+JSON_DIR = "/home/alex/dump1090-json"  # Make sure this folder exists and dump1090-fa has write permission
 JSON_FILE = f"{JSON_DIR}/aircraft.json"
 
 class ADSBApp(App):
-    def build(self):
-        self.label = Label(text="Starting dump1090-fa...", font_size='20sp')
-        # Start dump1090-fa as a subprocess
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.dump_process = None
+        self.label = None
+
+    def start_dump1090(self):
+        # Start dump1090-fa in background writing JSON
+        # Using sudo may be required depending on your setup
         self.dump_process = subprocess.Popen([
-            DUMP1090_PATH,
+            "sudo", DUMP1090_PATH,
             "--net",
             "--write-json", JSON_DIR,
             "--gain", "-10"
         ])
-        # Wait a moment to let dump1090 create files
-        time.sleep(2)
-        # Schedule periodic data updates
-        Clock.schedule_interval(self.update_data, 5)
-        return self.label
 
-    def update_data(self, dt):
-        try:
-            with open(JSON_FILE, "r") as f:
-                data = json.load(f)
-        except Exception as e:
-            self.label.text = f"Error reading JSON:\n{e}"
-            return
-
-        aircraft_list = data.get("aircraft", [])
-        if not aircraft_list:
-            self.label.text = "No aircraft detected."
-        else:
-            display_text = "Aircraft detected:\n"
-            for ac in aircraft_list[:10]:
-                flight = ac.get("flight", "N/A")
-                altitude = ac.get("altitude", "N/A")
-                display_text += f"{flight} @ {altitude} ft\n"
-            self.label.text = display_text
-
-    def on_stop(self):
-        # Make sure to terminate dump1090 process when the app closes
-        if self.dump_process.poll() is None:  # still running
+    def stop_dump1090(self):
+        if self.dump_process:
             self.dump_process.terminate()
             self.dump_process.wait()
 
-if __name__ == "__main__":
+    def build(self):
+        self.label = Label(text="Starting ADS-B...", font_size='20sp')
+        self.start_dump1090()
+
+        # Schedule periodic update every 2 seconds
+        Clock.schedule_interval(self.update_display, 2)
+        return self.label
+
+    def update_display(self, dt):
+        try:
+            with open(JSON_FILE, 'r') as f:
+                data = json.load(f)
+            aircraft_list = data.get("aircraft", [])
+            if not aircraft_list:
+                self.label.text = "No aircraft detected."
+            else:
+                display_text = "Aircraft detected:\n"
+                for ac in aircraft_list[:10]:
+                    flight = ac.get("flight")
+                    if not flight:
+                        flight = "Unknown Flight"
+                    altitude = ac.get("altitude")
+                    if altitude is None:
+                        altitude = "Unknown Altitude"
+                    else:
+                        altitude = f"{altitude} ft"
+                    display_text += f"{flight} @ {altitude}\n"
+                self.label.text = display_text
+        except Exception as e:
+            self.label.text = f"Error fetching data:\n{e}"
+
+    def on_stop(self):
+        # Stop dump1090 process on app exit
+        self.stop_dump1090()
+
+if __name__ == '__main__':
     ADSBApp().run()
